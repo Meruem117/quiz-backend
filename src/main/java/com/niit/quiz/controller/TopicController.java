32ff56ba150ad105556db1a270e48199eae70821
Meruem117 <1572677319@qq.com>
2022-01-10 21:45:57 +0800

    update topic

--- src/main/java/com/niit/quiz/controller/TopicController.java
+++ src/main/java/com/niit/quiz/controller/TopicController.java
@@ -1,6 +1,5 @@
 package com.niit.quiz.controller;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.niit.quiz.base.response.BaseResponse;
 import com.niit.quiz.base.response.ResultUtils;
 import com.niit.quiz.model.entity.Topic;
