566bdb204c24008702ed212687d2aa251d2d441d
Meruem117 <1572677319@qq.com>
2022-01-11 22:45:33 +0800

    update question controller

---
+++ src/main/java/com/niit/quiz/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package com.niit.quiz.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.niit.quiz.base.response.BaseResponse;
+import com.niit.quiz.base.response.ResultUtils;
+import com.niit.quiz.model.entity.Question;
+import com.niit.quiz.service.QuestionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+    @Resource
+    private QuestionService questionService;
+
+    @GetMapping("/get")
+    public BaseResponse<List<Question>> getQuestionListByTopicId(@RequestParam String topic) {
+        QueryWrapper<Question> questionQueryWrapper = new QueryWrapper<>();
+        questionQueryWrapper.eq("topic", topic);
+        return ResultUtils.success(questionService.list(questionQueryWrapper));
+    }
+}
