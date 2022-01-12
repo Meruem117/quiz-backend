dc72f2f1ac06b764c7ad6d52bc59d00856d7792f
Meruem117 <1572677319@qq.com>
2022-01-12 20:21:15 +0800

    update result

---
+++ src/main/java/com/niit/quiz/service/ResultService.java
@@ -0,0 +1,7 @@
+package com.niit.quiz.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.niit.quiz.model.entity.Result;
+
+public interface ResultService extends IService<Result> {
+}
---
+++ src/test/java/com/niit/quiz/service/ResultServiceTest.java
@@ -0,0 +1,11 @@
+package com.niit.quiz.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class ResultServiceTest {
+    @Resource
+    private ResultService resultService;
+}
