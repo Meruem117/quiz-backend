7db6c19ea004884d533c59c33522c01cd0bbba5d
Meruem117 <1572677319@qq.com>
2022-01-12 20:51:10 +0800

    add controller

---
+++ src/main/java/com/niit/quiz/controller/QuizController.java
@@ -0,0 +1,9 @@
+package com.niit.quiz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+}
---
+++ src/main/java/com/niit/quiz/controller/ScheduleController.java
@@ -0,0 +1,9 @@
+package com.niit.quiz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedule")
+public class ScheduleController {
+}
