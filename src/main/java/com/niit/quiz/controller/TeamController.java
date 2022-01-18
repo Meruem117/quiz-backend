c7115e0f65d0a3db390092d9b5840c7db8d66e09
Meruem117 <1572677319@qq.com>
2022-01-18 22:42:56 +0800

    add team controller

---
+++ src/main/java/com/niit/quiz/controller/TeamController.java
@@ -0,0 +1,27 @@
+package com.niit.quiz.controller;
+
+import com.niit.quiz.base.response.BaseResponse;
+import com.niit.quiz.base.response.ResultUtils;
+import com.niit.quiz.model.entity.Team;
+import com.niit.quiz.service.TeamService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+    @Resource
+    private TeamService teamService;
+
+    /**
+     * get team list
+     *
+     * @return team item list
+     */
+    public BaseResponse<List<Team>> getTeamList() {
+        return ResultUtils.success(teamService.list());
+    }
+}
