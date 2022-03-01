f6e97d683cc23fcf71160c28982776ec8abb3518
Meruem117 <1572677319@qq.com>
2022-03-01 21:11:30 +0800

    add swagger3

--- src/main/java/com/niit/quiz/QuizApplication.java
+++ src/main/java/com/niit/quiz/QuizApplication.java
@@ -3,8 +3,10 @@ package com.niit.quiz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @MapperScan("com.niit.quiz.mapper")
 public class QuizApplication {
 
---
+++ src/main/java/com/niit/quiz/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.niit.quiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.niit.quiz.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Quiz")
+                .version("1.0")
+                .description("Backend for a quiz system")
+                .build();
+    }
+}
