154b0548d797d190549b02514b9c059ad5b99ced
Meruem117 <1572677319@qq.com>
2022-05-13 22:10:27 +0800

    update member controller

--- src/main/java/com/niit/quiz/controller/MemberController.java
+++ src/main/java/com/niit/quiz/controller/MemberController.java
@@ -3,14 +3,13 @@ package com.niit.quiz.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.niit.quiz.base.exception.BaseException;
 import com.niit.quiz.base.exception.ErrorCodeEnum;
+import com.niit.quiz.base.request.DeleteRequest;
 import com.niit.quiz.base.response.BaseResponse;
-import com.niit.quiz.utils.ResultUtils;
 import com.niit.quiz.model.entity.Member;
+import com.niit.quiz.utils.DateUtils;
+import com.niit.quiz.utils.ResultUtils;
 import com.niit.quiz.service.MemberService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -27,7 +26,7 @@ public class MemberController {
      * @param id user id
      * @return member item list
      */
-    @GetMapping("/team")
+    @GetMapping("/member")
     public BaseResponse<List<Member>> getTeamListByUserId(@RequestParam int id) {
         if (id < 1) {
             throw new BaseException(ErrorCodeEnum.REQUEST_PARAMS_ERROR);
@@ -38,9 +37,9 @@ public class MemberController {
     }
 
     /**
-     * get a team's members
+     * get a member's members
      *
-     * @param id team id
+     * @param id member id
      * @return member item list
      */
     @GetMapping("/user")
@@ -51,5 +50,36 @@ public class MemberController {
         QueryWrapper<Member> memberQueryWrapper = new QueryWrapper<>();
         memberQueryWrapper.eq("team_id", id);
         return ResultUtils.success(memberService.list(memberQueryWrapper));
+    }
+
+    /**
+     * add member
+     *
+     * @param member member item
+     * @return member id
+     */
+    @PostMapping("/add")
+    public BaseResponse<Integer> addMember(@RequestBody Member member) {
+        if (member == null) {
+            throw new BaseException(ErrorCodeEnum.REQUEST_PARAMS_ERROR);
+        }
+        String date = DateUtils.getCurrentDateTime();
+        member.setJoinTime(date);
+        memberService.save(member);
+        return ResultUtils.success(member.getId());
+    }
+
+    /**
+     * logical delete member
+     *
+     * @param deleteRequest member id
+     * @return delete status
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteTeam(@RequestBody DeleteRequest deleteRequest) {
+        if (deleteRequest == null || deleteRequest.getId() < 1) {
+            throw new BaseException(ErrorCodeEnum.REQUEST_PARAMS_ERROR);
+        }
+        return ResultUtils.success(memberService.removeById(deleteRequest.getId()));
     }
 }
