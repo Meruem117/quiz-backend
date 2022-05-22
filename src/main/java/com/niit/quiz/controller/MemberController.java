68dcdede60c47a27467bde8b29358ef441ef24db
Meruem117 <1572677319@qq.com>
2022-05-22 22:38:20 +0800

    update member

--- src/main/java/com/niit/quiz/controller/MemberController.java
+++ src/main/java/com/niit/quiz/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.niit.quiz.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.niit.quiz.base.exception.BaseException;
 import com.niit.quiz.base.exception.ErrorCodeEnum;
 import com.niit.quiz.base.request.DeleteRequest;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/member")
@@ -113,17 +115,21 @@ public class MemberController {
         memberQueryWrapper.eq("team_id", member.getTeamId());
         memberQueryWrapper.eq("user_id", member.getUserId());
         Member originMember = memberService.getOne(memberQueryWrapper);
-        if (originMember == null) {
+        if (ObjectUtils.isNotNull(originMember)) {
+            if (Objects.equals(originMember.getQuit(), QuitEnum.NOT_QUIT.getValue())) {
+                return ResultUtils.error("You have already applied before");
+            } else {
+                originMember.setPass(PassEnum.PENDING.getValue());
+                originMember.setQuit(QuitEnum.NOT_QUIT.getValue());
+                memberService.updateById(originMember);
+                return ResultUtils.success(originMember.getId());
+            }
+        } else {
             String date = DateUtils.getCurrentDate();
             member.setJoinTime(date);
             member.setCreateTime(date);
             memberService.save(member);
             return ResultUtils.success(member.getId());
-        } else {
-            originMember.setPass(PassEnum.PENDING.getValue());
-            originMember.setQuit(QuitEnum.NOT_QUIT.getValue());
-            memberService.updateById(originMember);
-            return ResultUtils.success(originMember.getId());
         }
     }
 
